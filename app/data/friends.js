86fb4527cef218b14be5bf9541e8d1ee7a2a4088
erkartali <erkartali@gmail.com>
2020-05-21 22:04:36 -0500

    worked on html routes and api routes files

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendsArray = [
+    {
+      customerName: "Saima",
+      customerEmail: "saima@example.com",
+      phoneNumber: "000-000-0000",
+      customerID: "saimaCool"
+    }
+  ];
+  
+  module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-// var path = require("path");
+var path = require("path");
 
 var app = express();
 
@@ -8,25 +8,9 @@ var PORT = process.env.PORT || 3535;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
-
-module.exports = function(app) {
-  
-    app.get("/tables", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/tables.html"));
-    });
-  
-    app.get("/reserve", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/reserve.html"));
-    });
-  
-    // If no matching route is found default to home
-    app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-  };
