553c68e28242233c3bb32f101ea278841662213e
erkartali <erkartali@gmail.com>
2020-06-01 16:24:57 -0500

    updated the formatting of some pages

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,28 +1,23 @@
-var friendsArray = [
-  {
-    name: "Martha Sewart",
-    pic:
-      "https://i1.wp.com/www.usmagazine.com/wp-content/uploads/2020/04/Martha-Stewart-Shares-Her-Fun-and-Festive-Easter-Tradition-Feature.jpg?crop=0px%2C16px%2C1583px%2C894px&resize=1000%2C563&ssl=1",
-    enteredData: ["3", "1", "4", "2", "4", "1", "2", "3", "5", "3"],
-  },
-  {
-    name: "Snoop Dog",
-    pic:
-    "https://www.kuvo.org/wp-content/uploads/2019/11/snoop-dogg-i-wanna-thank-me-interview.jpg",
-    enteredData: ["1", "4", "2", "1", "5", "3", "1", "2", "4", "2"],
-  },
-  {
-    name: "Barack Obama",
-    pic:
-      "https://www.rollingstone.com/wp-content/uploads/2020/06/Barack-Obama.jpg?resize=1800,1200&w=1200",
-    enteredData: ["2", "4", "3", "1", "3", "4", "2", "1", "1", "4"],
-  },
-  {
-    name: "Mother Teresa",
-    pic:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Mother_Teresa_1.jpg/800px-Mother_Teresa_1.jpg",
-    enteredData: ["3", "5", "2", "3", "1", "4", "3", "1", "4", "3"],
-  },
+var friendsArray = [{
+        name: "Martha Sewart",
+        pic: "https://i1.wp.com/www.usmagazine.com/wp-content/uploads/2020/04/Martha-Stewart-Shares-Her-Fun-and-Festive-Easter-Tradition-Feature.jpg?crop=0px%2C16px%2C1583px%2C894px&resize=1000%2C563&ssl=1",
+        enteredData: ["3", "1", "4", "2", "4", "1", "2", "3", "5", "3"],
+    },
+    {
+        name: "Snoop Dog",
+        pic: "https://www.kuvo.org/wp-content/uploads/2019/11/snoop-dogg-i-wanna-thank-me-interview.jpg",
+        enteredData: ["1", "4", "2", "1", "5", "3", "1", "2", "4", "2"],
+    },
+    {
+        name: "Barack Obama",
+        pic: "https://www.rollingstone.com/wp-content/uploads/2020/06/Barack-Obama.jpg?resize=1800,1200&w=1200",
+        enteredData: ["2", "4", "3", "1", "3", "4", "2", "1", "1", "4"],
+    },
+    {
+        name: "Mother Teresa",
+        pic: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Mother_Teresa_1.jpg/800px-Mother_Teresa_1.jpg",
+        enteredData: ["3", "5", "2", "3", "1", "4", "3", "1", "4", "3"],
+    },
 ];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,46 +1,46 @@
 var friendData = require("../data/friends");
 var fs = require("fs");
 
-module.exports = function (app) {
-	app.get("/api/friends", function (req, res) {
-		res.json(friendData);
-	});
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
+    });
 
-	app.post("/api/friends", function (req, res) {
-		console.log(req.body);
+    app.post("/api/friends", function(req, res) {
+        console.log(req.body);
 
-		var newPerson = req.body;
-		var newFriendTotal = 0;
-		var otherFriend = 0;
-		var bestFriend = 1000;
-		var bestFriendData = {};
+        var newPerson = req.body;
+        var newFriendTotal = 0;
+        var otherFriend = 0;
+        var bestFriend = 1000;
+        var bestFriendData = {};
 
-		for (let index = 0; index < friendData.length; index++) {
-			newFriendTotal = 0;
-			otherFriend = 0;
+        for (let index = 0; index < friendData.length; index++) {
+            newFriendTotal = 0;
+            otherFriend = 0;
 
-			for (let i = 0; i < friendData[index].enteredData.length; i++) {
-				newFriendTotal += Number(newPerson.enteredData[i]);
-				otherFriend += Number(friendData[index].enteredData[i]);
-			}
-			console.log("nft: ", newFriendTotal, "of:", otherFriend);
-			if (Math.abs(newFriendTotal - otherFriend) < bestFriend) {
-				bestFriend = Math.abs(newFriendTotal - otherFriend);
-				bestFriendData = friendData[index];
-				console.log("bfd:", bestFriendData);
-			}
-		}
+            for (let i = 0; i < friendData[index].enteredData.length; i++) {
+                newFriendTotal += Number(newPerson.enteredData[i]);
+                otherFriend += Number(friendData[index].enteredData[i]);
+            }
+            console.log("nft: ", newFriendTotal, "of:", otherFriend);
+            if (Math.abs(newFriendTotal - otherFriend) < bestFriend) {
+                bestFriend = Math.abs(newFriendTotal - otherFriend);
+                bestFriendData = friendData[index];
+                console.log("bfd:", bestFriendData);
+            }
+        }
 
-		friendData.push(req.body);
+        friendData.push(req.body);
 
-		// var getData = `var friendsArray = ${JSON.parse(friendData)}
-		// module.exports = friendsArray;
-		// `
+        // var getData = `var friendsArray = ${JSON.parse(friendData)}
+        // module.exports = friendsArray;
+        // `
 
-		// console.log(getData);
-		// fs.writeFileSync('./friends.js', getData, function(err, data) {
-		//   console.log(data)
-		// })
-		res.json(bestFriendData);
-	});
+        // console.log(getData);
+        // fs.writeFileSync('./friends.js', getData, function(err, data) {
+        //   console.log(data)
+        // })
+        res.json(bestFriendData);
+    });
 };
